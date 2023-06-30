88d395f1c03d42cdd388f5b7eaf0c137fb497d62
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-06-30 22:26:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ provider "azurerm" {
   features {}
 
   subscription_id   = "7263089f-accc-497d-b874-d31cf3d0dec4"
+  tenant_id         = "5e0fd201-009a-46c0-bbb9-a2f273190dce"
+  client_id         = "suhas.deepala@cloudexpertsguru.com"
+  client_secret     = "Drk@69275"
+
 }
 
 resource "azurerm_resource_group" "rg" {
